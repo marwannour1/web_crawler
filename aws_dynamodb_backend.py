75b6f9a3ad2eba8033b8a4ca8aa1a2553caf8cb0
marwannour1 <marwannour20022016@gmail.com>
2025-04-24 02:06:39 +0200

    backend

--- aws_dynamodb_backend.py
+++ aws_dynamodb_backend.py
@@ -183,5 +183,5 @@ class DynamoDBBackend(KeyValueStoreBackend):
         return json.loads(base64.b64decode(data.encode()).decode())
 
 # Register the backend
-from celery.backends.base import Backend
-Backend.register('dynamodb', 'aws_dynamodb_backend.DynamoDBBackend')
+from celery import backends
+backends.BACKEND_ALIASES['dynamodb'] = 'aws_dynamodb_backend.DynamoDBBackend'
